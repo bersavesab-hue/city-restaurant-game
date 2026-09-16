8d3a1868e4f6e50054090c5290a3773ab73acc32
bersavesab-hue <bersavesab@gmail.com>
2026-09-16 16:29:03 +0800

    add phase2 food quality data foundation

---
+++ src/data/foodSystemConfig.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  qualityLevels: [
+    { id: 1, name: '普通', multiplier: 1 },
+    { id: 2, name: '优秀', multiplier: 1.15 },
+    { id: 3, name: '精品', multiplier: 1.35 },
+    { id: 4, name: '招牌', multiplier: 1.7 },
+    { id: 5, name: '传奇', multiplier: 2.2 }
+  ],
+  categories: [
+    '主食',
+    '热菜',
+    '凉菜',
+    '饮品',
+    '甜品'
+  ]
+};
