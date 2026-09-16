cdd6475bebf306a889e68626c58883cd9dc2c74c
bersavesab-hue <bersavesab@gmail.com>
2026-09-16 16:33:04 +0800

    Add restaurant systems test data

---
+++ src/testing/restaurantSystemsTestData.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const sampleRestaurantData = {
+  dishes: [
+    {
+      name: '招牌家常菜',
+      quality: '精品',
+      cost: 18,
+      price: 58
+    }
+  ],
+  staff: [
+    {
+      name: '厨师',
+      level: '熟练员工',
+      cooking: 70
+    }
+  ],
+  rating: {
+    score: 82,
+    level: '精品店'
+  }
+};
+
+module.exports = sampleRestaurantData;
