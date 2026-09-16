becc4932a1bde5898c23e52a45bcb4065a54962f
bersavesab-hue <bersavesab@gmail.com>
2026-09-16 16:29:10 +0800

    add phase2 employee growth foundation

---
+++ src/data/employeeSystemConfig.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  attributes: [
+    'cooking',
+    'service',
+    'efficiency',
+    'stability',
+    'innovation'
+  ],
+  levels: [
+    '新人',
+    '普通员工',
+    '熟练员工',
+    '高级员工',
+    '主管',
+    '店长'
+  ]
+};
